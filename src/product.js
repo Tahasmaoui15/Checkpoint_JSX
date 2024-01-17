1a5d7aeaf0b7fa95ea00d14c80babdc0d16d6150
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-17 18:07:17 +0100

    Fixed typing error in product.js

--- src/product.js
+++ src/product.js
@@ -1,10 +1,10 @@
 // product.js
 
 const product = {
-    name: "Awesome Product",
+    name: "",
     price: 19.99,
-    description: "A description of the awesome product.",
-    image: "https://example.com/product-image.jpg",
+    description: "",
+    image: "",
   };
   
   export default product;
