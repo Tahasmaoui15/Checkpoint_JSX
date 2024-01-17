e6103233b655fbb432f182c11b965816727201bd
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-17 18:05:06 +0100

    fixed comments in product.js

--- src/product.js
+++ src/product.js
@@ -1,11 +1,11 @@
-// // product.js
+// product.js
 
-// const product = {
-//     name: "Awesome Product",
-//     price: 19.99,
-//     description: "A description of the awesome product.",
-//     image: "https://example.com/product-image.jpg",
-//   };
+const product = {
+    name: "Awesome Product",
+    price: 19.99,
+    description: "A description of the awesome product.",
+    image: "https://example.com/product-image.jpg",
+  };
   
-//   export default product;
+  export default product;
   
